470aa104a6cb7b71c5a0dd531d5bb38fd1c56ee8
Mamba-cita <EDWINSJOHN52@gmail.com>
2024-07-01 16:14:28 +0300

    revert methods

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,9 +10,9 @@ import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/auth", authUser);
-router.post("/register", registerUser);
-router.post("/logout", logoutUser);
+router.all("/auth", authUser);
+router.all("/register", registerUser);
+router.all("/logout", logoutUser);
 router.route("/profile").get(protect ,getUserProfile).put(protect ,updateUserProfile);
 
 export default router;
