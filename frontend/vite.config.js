ec141cff443c1b68275160cadf4f94b84f1cb3da
Mamba-cita <EDWINSJOHN52@gmail.com>
2024-07-01 15:45:58 +0300

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -26,7 +26,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    open: true,
+    // open: true,
     proxy: {
       '/api': {
         target: 'https://t-truck.onrender.com',
