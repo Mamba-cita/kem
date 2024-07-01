5aead173c4268eb65b8a19a9ed4fe83c403eb630
Mamba-cita <EDWINSJOHN52@gmail.com>
2024-07-01 14:41:02 +0300

    add prod config to vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     // open: true,
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:8001',
+//         changeOrigin: true,
+//       },
+//     },
+//   }
+// })
+
+
+
+//production
+
+
+// https://vitejs.prod/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -9,7 +29,7 @@ export default defineConfig({
     // open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:8001',
+        target: 'https://t-truck.onrender.com',
         changeOrigin: true,
       },
     },
